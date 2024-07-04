be1ecdd3c9a43348e7f9ecfc06eecdc6a0b1d1d6
mohamed mosilhy <93820559+mohamedmosilhy@users.noreply.github.com>
2024-07-05 00:08:21 +0300

    Delete lib/core/secrets directory

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,5 +0,0 @@
-class AppSecrets {
-  static const supabaseUrl = "https://thczridcfetdjjswmavj.supabase.co";
-  static const supabaseAnon =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRoY3pyaWRjZmV0ZGpqc3dtYXZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk1NTM5MzAsImV4cCI6MjAzNTEyOTkzMH0.MxreXbzKh6pYLVjUolOxd1USAhpjsR587-5snbyAb18";
-}
